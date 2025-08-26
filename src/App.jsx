5514ea59f2be52fc09f37c4b08bd44fdb7d3ff04
James Heaton <heaton1138@gmail.com>
2025-08-26 13:20:45 -0500

    ticketService.jsx & app.css created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react";
+import { getAllTickets } from "./services/ticketService";
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
-}
+  const [allTickets, setAllTickets] = useState([])
+
+  // useEffect takes 2 arguments: a function & an array (dependency array)
+  useEffect(() => {
+    getAllTickets().then(ticketsArray => {
+    setAllTickets(ticketsArray)
+  })
+  }, []) // the function is what we want to happen // the array is when it will happen // empty array only runs on initial render of component
+
+  
+
+  return <></>;
+};
